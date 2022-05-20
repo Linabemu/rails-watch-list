da53c1837a01d1b60294cdc0b2d503d44dfb566f
Lina Becerra <lina.bemu@gmail.com>
2022-05-20 16:10:53 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'lists#index'
-  resources :lists do
+  root to: 'lists#index'
+  resources :lists, except: :index do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
